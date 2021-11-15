a1fc3accb745258e37a4e7f0bce6e601a34d5fac
Slotus2004 <94376600+Slotus2004@users.noreply.github.com>
2021-11-15 22:32:41 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `Tau có điều này muốn hỏi Ngưi, Ngưi nhớ phải trả lời thật lòng nhaaa`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'Tau biết Mi thích Tau cóa phải khum phải khum ? 😙',
+    desc: 'Mi không trả lời hoặc thoát ra, tức là Mi thích Tau đó nha muhahahaha :> ',
+    btnYes: 'Đúmg, tau thật sự thích mii 💖💖💖',
+    btnNo: '💔 Mi mơ à 🙂 ! 💔 ',
+    question:'Nói cho Tau biết đi vì sao Mi thích tau 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    reply: 'Tại vì mii dễ thưn đóaaa Ahiiiii 😊😊',
+    mess: 'Tau biết mà, Tau cũng thích Mi lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Tau nhaaaaa. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    messLink: 'https://www.facebook.com/thisisslotus243/'
 }
